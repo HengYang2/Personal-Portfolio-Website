5930baf7bf1422584d2f4ddc8ee29e71b12f6edf
Heng Yang <hengya03@gmail.com>
2023-08-14 11:04:47 -0500

    Deleted unused files.

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as THREE from 'three'
+
 
 export default function App(props) {
 
@@ -11,11 +12,15 @@ export default function App(props) {
 
   const mainCamera = props.mainCamera
 
+  // const [isOpen, setIsOpen] = useState(flase);
 
   // props.camera1.position.set(props.camera1.position.x, props.camera1.position.y, props.camera1.position.z +1)
   return (
     <>
-      <div className='bg-blue-500 text-white absolute z-50 w-500 h-500' onClick={() => { target.position.set(-5, 1, 3); tweenLaptop.start(); console.log('clicked') }}>
+  {/* <Modal setIsOpen={setIsOpen} isOpen={isOpen}> */}
+    {/* Fancy Modal */}
+    {/* </Modal> */}
+      <div className='bg-blue-500 text-white absolute z-50 w-500 h-500' onClick={() => { target.position.set(-5, 1, 3); tweenLaptop.start(); console.log('clicked'); setIsOpen={setIsOpen}}}>
         Laptop
       </div>
       <div className='bg-blue-500 text-white absolute z-50 w-500 h-500 mt-12' onClick={() => { target.position.set(-6, 1.5, -0.5); tweenShelf.start(); console.log('clicked') }}>
